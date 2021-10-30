bab2f0f79081534b9b3c73e0386c97ffe425643d
Wipeshot <79708519+Wipeshot@users.noreply.github.com>
2021-10-30 11:49:21 +0200

    HintTextField + HintPasswordField not anymore in GUI, Main changed GUI object name

--- src/me/mustache/main/Main.java
+++ src/me/mustache/main/Main.java
@@ -8,9 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Database.getInstance().setUrl("Notenrechner");
-        Gui gui = new Gui();
-
-
+        new Gui();
     }
 
 }
